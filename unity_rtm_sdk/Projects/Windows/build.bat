5fd12a5d6717f080e0e2f5e5d5bbd33ac64db448
Yiqing Huang <huangyiqing@agora.io>
2021-04-12 11:53:42 +0800

    Fix bugs caused by relative path

--- unity_rtm_sdk/Projects/Windows/build.bat
+++ unity_rtm_sdk/Projects/Windows/build.bat
@@ -9,7 +9,6 @@
 ::=============================================================================
 echo off
 SET OUTPUT_PACKAGE=RTM_windows.zip
-SET URL_CONFIG=%CURDIR%\url_config.txt
 mkdir agoraRTMCWrapper\sdk
 CALL :download_library x86
 echo x86 download_library error=%ERRORLEVEL%
@@ -17,13 +16,15 @@ CALL :download_library x64
 echo x64 download_library error=%ERRORLEVEL%
 
 REM Preparing step
-cd agoraRTMCWrapper
 SET ARCH=x86
-for /F "delims=" %%a in ('python ..\GetUrl.py %ARCH% filename') do SET ZIP_FILE=%%a
+for /F "delims=" %%a in ('python .\GetUrl.py %ARCH% filename') do SET ZIP_FILE=%%a
+cd agoraRTMCWrapper
 powershell -command "& .\prep-win-project.bat %ARCH% ..\%ZIP_FILE%"
+cd ..
 echo -------------- prep %ARCh% error=%ERRORLEVEL%
 SET ARCH=x64
-for /F "delims=" %%a in ('python ..\GetUrl.py %ARCH% filename') do SET ZIP_FILE=%%a
+for /F "delims=" %%a in ('python .\GetUrl.py %ARCH% filename') do SET ZIP_FILE=%%a
+cd agoraRTMCWrapper
 powershell -command "& .\prep-win-project.bat %ARCH% ..\%ZIP_FILE%"
 echo -------------- prep %ARCh% error=%ERRORLEVEL%
 
