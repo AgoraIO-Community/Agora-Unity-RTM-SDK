a19e1f1d94eacb726ed594bb35684d9e4a9a8a49
Yiqing Huang <huangyiqing@agora.io>
2021-04-12 11:35:56 +0800

    Fix a typo

--- unity_rtm_sdk/Projects/Windows/GetUrl.py
+++ unity_rtm_sdk/Projects/Windows/GetUrl.py
@@ -3,7 +3,7 @@ import sys
 
 arch = sys.argv[1]
 config = open('../../url_config.txt', 'r').read()
-idx = config.find('WIN_' + {arch})
+idx = config.find('WIN_' + arch)
 url = config[config.find('=', idx) + 1: config.find('\n', idx)].strip()
 
 try:
