87288ccda16359edf6c7f398e8ba32aa095c5743
zhangtao <zhangtao@agora.io>
2021-04-14 20:36:24 +0800

    fix iOS compile error

--- unity_rtm_sdk/Projects/IOS/agoraRTMCWrapper/common.h
+++ unity_rtm_sdk/Projects/IOS/agoraRTMCWrapper/common.h
@@ -15,8 +15,8 @@
 #include "../../../include/IAgoraRtmCallManager.h"
 #include "../../../include/IAgoraRtmService.h"
 #elif defined(__APPLE__)
-#include "../Agora_RTM_SDK_for_iOS/libs/AgoraRtmKit.all.framework/for_iphone/AgoraRtmKit.framework/Headers/IAgoraRtmCallManager.h"
-#include "../Agora_RTM_SDK_for_iOS/libs/AgoraRtmKit.all.framework/for_iphone/AgoraRtmKit.framework/Headers/IAgoraRtmService.h"
+#include "../Agora_RTM_SDK_for_iOS/libs/AgoraRtmKit.xcframework/ios-arm64_armv7/AgoraRtmKit.framework/Headers/IAgoraRtmCallManager.h"
+#include "../Agora_RTM_SDK_for_iOS/libs/AgoraRtmKit.xcframework/ios-arm64_armv7/AgoraRtmKit.framework/Headers/IAgoraRtmService.h"
 #elif defined(__ANDROID__) || defined(__linux__)
 #include "../prebuilt/include/IAgoraRtmCallManager.h"
 #include "../prebuilt/include/IAgoraRtmService.h"
